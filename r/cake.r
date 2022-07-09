a67eac55622ced905faa79cad1ad921def76ac0c
Agidza <50583599+lynnagidza@users.noreply.github.com>
2022-07-09 20:49:41 +0000

    final

--- r/cake.r
+++ r/cake.r
@@ -1,17 +1,16 @@
-flavours = matrix(c("Gin","Wine","Rum","Whiskey","Baileys","Whiskey","Gin","Rum","Baileys","Wine","Wine","Whiskey","Baileys","Rum","Gin","Gin","Whiskey","Wine","Baileys","Rum","Wine","Whiskey","Rum","Gin","Baileys","Gin","Wine","Whiskey","Rum","Baileys","Whiskey","Gin","Rum","Wine","Baileys","Gin","Rum","Baileys","Wine","Whiskey"), 
+flavours <- matrix(c("Gin","Wine","Rum","Whiskey","Baileys","Whiskey","Gin","Rum","Baileys","Wine","Wine","Whiskey","Baileys","Rum","Gin","Gin","Whiskey","Wine","Baileys","Rum","Wine","Whiskey","Rum","Gin","Baileys","Gin","Wine","Whiskey","Rum","Baileys","Whiskey","Gin","Rum","Wine","Baileys","Gin","Rum","Baileys","Wine","Whiskey"), 
 nrow=8, 
 ncol=5,  
-  byrow = TRUE         
+byrow = TRUE         
 )
 
-rownames(flavours) = c("M N", "T O", "F M", "K M", "B K", "C M", "D G","A W")
-colnames(flavours) = c("1", "2", "3", "4", "5")
+rownames(flavours) <- c("M N", "T O", "F M", "K M", "B K", "C M", "D G","A W")
+colnames(flavours) <- c("1", "2", "3", "4", "5")
   
 print(flavours)
 getmode <- function(fave) {
  theone <- unique(fave)
  theone[which.max(tabulate(match(fave, theone)))]
 }
-winner = getmode(flavours)
-cat("People's choice: ")
-print(winner)
+winner <- getmode(flavours)
+paste("People's choice:",winner)
